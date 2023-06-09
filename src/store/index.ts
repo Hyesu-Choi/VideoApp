14100cf964ff03722189da1826e3e5f46bd7a407
HyesuChoi <chlgotn0580@naver.com>
2023-06-09 13:41:56 +0900

    youtube clone project
    all code

---
+++ src/store/index.ts
@@ -0,0 +1,63 @@
+import { configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { InitialState } from "../Types";
+import { getHomePageVideos } from "./reducers/getHomePageVideos";
+import { getSearchPageVideos } from "./reducers/getSearchPageVideos";
+import { getVideoDetails } from "./reducers/getVideoDetails";
+import { getRecommendedVideos } from "./reducers/getRecommendedVideos";
+
+// 앱 초기 상태 값 정의
+const initialState: InitialState = {
+  videos: [],
+  currentPlaying: null,
+  searchTerm: "",
+  searchResults: [],
+  nextPageToken: null,
+  recommendedVideos: [],
+};
+
+const YoutubeSlice = createSlice({
+  name: "youtubeApp",
+  initialState,
+  reducers: {
+    clearVideos: (state) => {
+      state.videos = [];
+      state.nextPageToken = null;
+    },
+    changeSearchTerm: (state, action: PayloadAction<string>) => {
+      state.searchTerm = action.payload;
+    },
+    clearSearchTerm: (state) => {
+      state.searchTerm = "";
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder.addCase(getHomePageVideos.fulfilled, (state, action) => {
+      state.videos = action.payload.parsedData;
+      state.nextPageToken = action.payload.nextPageToken;
+    });
+    builder.addCase(getSearchPageVideos.fulfilled, (state, action) => {
+      state.videos = action.payload.parsedData;
+      state.nextPageToken = action.payload.nextPageToken;
+    });
+    builder.addCase(getVideoDetails.fulfilled, (state, action) => {
+      state.currentPlaying = action.payload;
+    });
+    builder.addCase(getRecommendedVideos.fulfilled, (state, action) => {
+      state.recommendedVideos = action.payload.parsedData;
+    });
+  },
+});
+
+export const store = configureStore({
+  reducer: {
+    youtubeApp: YoutubeSlice.reducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export const { clearVideos, changeSearchTerm, clearSearchTerm } =
+  YoutubeSlice.actions;
